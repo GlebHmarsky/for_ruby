399b8dd2c0179963603f14da99e9fc9417b7dd74
GlebHmarsky <hmarsky.gleb@yandex.ru>
2020-03-30 10:46:45 +0300

    Починил машину

---
+++ Car/hello.rb
@@ -0,0 +1,10 @@
+require_relative "my_car"
+
+def main
+    car1 = Car.new("")
+
+end
+
+if __FILE__ == $0
+
+end
--- Car/main.rb
+++ Car/main.rb
@@ -1,1 +1,19 @@
-puts "Hello World"
+require_relative "my_car"
+require_relative "fleet"
+def main
+    fleet = Fleet.new
+    fleet.load_from_file("cars-list.json")
+    
+    puts fleet.average_consumption
+
+    puts fleet.number_by_brand("BMW");
+
+    puts fleet.number_by_model("X5 3,0D")
+
+    puts fleet.consumption_by_brand("BMW")
+end
+
+
+if __FILE__ == $0
+    main
+end
