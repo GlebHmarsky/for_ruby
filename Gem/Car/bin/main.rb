0d0e1848584f0ca111c169191a5fd9e2d0ebb663
GlebHmarsky <hmarsky.gleb@yandex.ru>
2020-04-06 11:21:18 +0300

    Gem with car

---
+++ Gem/Car/bin/main.rb
@@ -0,0 +1,19 @@
+require_relative "../lib/my_car"
+require_relative "../lib/fleet"
+def main
+    fleet = Fleet.new
+    fleet.load_from_file("../data/cars-list.json")
+    
+    puts fleet.average_consumption
+
+    puts fleet.number_by_brand("BMW");
+
+    puts fleet.number_by_model("X5 3,0D")
+
+    puts fleet.consumption_by_brand("BMW")
+end
+
+
+if __FILE__ == $0
+    main
+end
