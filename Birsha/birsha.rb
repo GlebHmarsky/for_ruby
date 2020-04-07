a75023364192f5b44ffc9284125343a06552a25e
GlebHmarsky <hmarsky.gleb@yandex.ru>
2020-04-08 00:14:56 +0300

    Перед смертью не надышешься

--- Birsha/birsha.rb
+++ Birsha/birsha.rb
@@ -1,47 +1,51 @@
+# frozen_string_literal: true
 
 def float?(str)
-    Float(str) rescue false
+  Float(str)
+rescue StandardError
+  false
 end
-if(ARGV.size ==0 )
-    printf "Приложение эмитирует игру на рынке. Передайте стартовый капитал и количество дней в качестве аргументов приложения.
-    
+if ARGV.empty?
+  printf "Приложение эмитирует игру на рынке. Передайте стартовый капитал
+   и количество дней в качестве аргументов приложения.
+
 birsha.rb CAPITAL DAYS"
-    return
+  return
 elsif ARGV.size == 1
-    printf "НЕ ДОСТАТОЧНО АРГУМЕЕЕЕНТОВ!"
-    return
+  printf 'НЕ ДОСТАТОЧНО АРГУМЕЕЕЕНТОВ!'
+  return
 elsif ARGV.size > 2
-    printf "многовато аргументов!"
-end 
+  printf 'многовато аргументов!'
+end
 
 c = ARGV[0]
-if(!volume=float?(c)) then
-    p "Ругаться!1"
-else 
-    printf "Start invests: #{volume}\n"
+if !volume = float?(c)
+  p 'Ругаться!1'
+else
+  printf "Start invests: #{volume}\n"
 end
 
 c = ARGV[1]
-if(!days=float?(c)) then
-    p "Ругаться!2"
-else 
-    printf "Days: #{days.to_i}\n"
+if !days = float?(c)
+  p 'Ругаться!2'
+else
+  printf "Days: #{days.to_i}\n"
 end
 
 days.to_i.times do
-    rnd = rand(16)
-    if rnd > 14
-        volume *= 1.1
-    elsif rnd > 12
-        volume *= 1.02
-    elsif rnd > 9
-        volume *= 1
-    elsif rnd > 7
-        volume *= 0.98
-    elsif rnd >5
-        volume *= 0.9
-    else 
-        volume *=0.5
-    end
-    printf "#{rnd} #{volume} \n"
+  rnd = rand(16)
+  volume *= if rnd > 14
+              1.1
+            elsif rnd > 12
+              1.02
+            elsif rnd > 9
+              1
+            elsif rnd > 7
+              0.98
+            elsif rnd > 5
+              0.9
+            else
+              0.5
+            end
+  printf "#{rnd} #{volume} \n"
 end
--- for_tests/main.rb
+++ for_tests/main.rb
@@ -1,9 +1,20 @@
+def float?(str)
+	Float(str) rescue false
+end
+
 def main
-    if ()
-        pp 1
-    else
-        pp 2
-    end
+	printf "Це калькуль, давай учиться считать вместе\n>"
+	
+	num = sum = 0
+	while num != nil && num
+		if !(num = float?(gets))
+			next 
+		end
+		pp num
+		sum += num
+		pp sum
+	end
+	
 end
 
 main if __FILE__ == $0
