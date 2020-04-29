ae2d13989a6a2625e2b19763a32f32c661a1698e
GlebHmarsky <hmarsky.gleb@yandex.ru>
2020-04-29 09:23:33 +0300

    :)

--- ERB/bin/main.rb
+++ ERB/bin/main.rb
@@ -1,14 +1,40 @@
-# frozen_string_literal: true
-
-require_relative '../lib/obj_creator.rb'
-
 require 'erb'
+def hello_erb(chel)
+  temp = ERB.new "Hello <%= chel %>"
+  temp.result(binding)
+end
 
-weekday = Time.now.strftime('%A')
-simple_template = "Today is <%= weekday %>."
+def hello(chel)
+  puts "Hello #{chel}"
+end
 
-renderer = ERB.new(simple_template)
-puts renderer.result()
+def invite_erb(name,autor,what,text)
+  template = "Hello <%= name %>
+  приглашаем на <%= what%>
+   <%=text %> 
+   С уважением, <%=autor %>"
+   renderer = ERB.new(template)
+   renderer.result(binding)
+end
 
-# rc = Creator.new
-# rc.create
+def buy_tovar(name,price,kat)
+  template = "<% if kat == :dress %>
+  Ну купи платье
+  Название: <%=name%>
+  Цена сия чуда: <%=price%>
+  <% elsif kat == :tools %>
+    Ну купи штучки
+    Название: <%=name%>
+    Цена чуда-юда: <%=price%>
+    <% end %>"
+  renderer = ERB.new(template)
+  renderer.result(binding)
+end
+
+
+# puts hello_erb("Petr")
+# hello("Журик")
+
+# puts invite_erb("кожанный человек","белый пушистый","Гачисходка","Будет весело")
+
+puts buy_tovar("Mango",9999,:tools)
